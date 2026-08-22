62004e4c3e3c53c781412b4c8e17e00c5e3d04fb
Fulmine1000 <simone.gionta93@gmail.com>
2026-08-22 22:46:30 +0200

    Integra TV LG webOS nel gestore dispositivi

--- moduli/dispositivi_modulo.py
+++ moduli/dispositivi_modulo.py
@@ -1,311 +1,75 @@
 from dispositivi.gestore import GestoreDispositivi
-
 from dispositivi.computer import ComputerJarvis
 from dispositivi.rete import Rete
 from dispositivi.bluetooth import Bluetooth
 from dispositivi.telefono import TelefonoJarvis
 from dispositivi.connessione import ConnessioneDispositivi
 from dispositivi.smart_home import SmartHomeJarvis
-
-
-
+from dispositivi.tv_lg import LgWebOSTv
 
 
 class ModuloDispositivi:
-
+    """Registra e coordina tutti i dispositivi conosciuti da Jarvis."""
 
     def __init__(self, kernel):
-
         self.kernel = kernel
-
         self.nome = "Dispositivi"
-
         self.attivo = False
-
         self.gestore = None
-
         self.connessione = None
 
-
-
-
-
-
-
     def avvia(self):
-
-
         logger = self.kernel.logger
-
-
-
-        # CONNESSIONE DISPOSITIVI
-
-        self.connessione = ConnessioneDispositivi(
-
-            logger
-
-        )
-
-
+        self.connessione = ConnessioneDispositivi(logger)
         self.connessione.avvia()
+        self.gestore = GestoreDispositivi(logger)
+        self.gestore.collega_connessione(self.connessione)
 
+        for nome, dispositivo in (
+            ("computer", ComputerJarvis()),
+            ("rete", Rete()),
+            ("bluetooth", Bluetooth()),
+            ("smart_home", SmartHomeJarvis()),
+        ):
+            self.gestore.registra(nome, dispositivo)
 
-
-
-
-
-
-        # GESTORE CENTRALE
-
-        self.gestore = GestoreDispositivi(
-
-            logger
-
+        # TV LG webOS: integrazione reale solo se configurata; mai bloccare Jarvis.
+        tv_cfg = self.kernel.config.sezione("tv_lg")
+        tv = LgWebOSTv(
+            name=tv_cfg.get("nome", "tv"),
+            ip=tv_cfg.get("ip") or None,
+            client_key=tv_cfg.get("client_key") or None,
+            timeout=int(tv_cfg.get("timeout", 4)),
+            logger=logger,
         )
-
-
-        self.gestore.collega_connessione(
-
-            self.connessione
-
-        )
-
-
-
-
-
-
-
-        # COMPUTER
-
-        computer = ComputerJarvis()
-
-
-        self.gestore.registra(
-
-            "computer",
-
-            computer
-
-        )
-
-
-
-
-
-
-
-        # RETE
-
-        rete = Rete()
-
-
-        self.gestore.registra(
-
-            "rete",
-
-            rete
-
-        )
-
-
-
-
-
-
-
-        # BLUETOOTH
-
-        bluetooth = Bluetooth()
-
-
-        self.gestore.registra(
-
-            "bluetooth",
-
-            bluetooth
-
-        )
-
-        # SMART HOME
-
-        smart_home = SmartHomeJarvis()
-
-
-        self.gestore.registra(
-
-            "smart_home",
-
-            smart_home
-
-        )
-
-
-
-
-
-
-
-        # ==========================
-        # BASE UFFICIALE MOTOROLA
-        # ==========================
-
+        self.gestore.registra("tv", tv)
 
         motorola = TelefonoJarvis(
-
-            nome="Motorola",
-
-            modello="Motorola Base J.A.R.V.I.S.",
-
-            base=True,
-
-            logger=logger
-
+            nome="Motorola", modello="Motorola Base J.A.R.V.I.S.", base=True, logger=logger
         )
-
-
-
-        self.gestore.registra(
-
-            "motorola",
-
-            motorola
-
-        )
-
-
-
-        self.connessione.connetti(
-
-            "motorola"
-
-        )
-
-
-
-
-
-
-
-        # ==========================
-        # TELEFONO SECONDARIO
-        # ==========================
-
+        self.gestore.registra("motorola", motorola)
+        self.connessione.connetti("motorola")
 
         telefono = TelefonoJarvis(
-
-            nome="Telefono",
-
-            modello="Dispositivo Android",
-
-            base=False,
-
-            logger=logger
-
+            nome="Telefono", modello="Dispositivo Android", base=False, logger=logger
         )
-
-
-
-        self.gestore.registra(
-
-            "telefono",
-
-            telefono
-
-        )
-
-
-
-        self.connessione.connetti(
-
-            "telefono"
-
-        )
-
-
-
-
-
-
+        self.gestore.registra("telefono", telefono)
+        self.connessione.connetti("telefono")
 
         self.attivo = True
-
-
-
-        logger.info(
-
-            "Modulo Dispositivi J.A.R.V.I.S. OS 3.0 attivo."
-
-        )
-
-
-
+        logger.info("Modulo Dispositivi J.A.R.V.I.S. 3.1 attivo.")
         return True
 
-
-
-
-
-
-
     def get_gestore(self):
-
         return self.gestore
 
-
-
-
-
-
-
     def get_connessione(self):
-
         return self.connessione
 
-
-
-
-
-
-
     def stato(self):
-
-
         return {
-
-
-            "nome":
-
-                self.nome,
-
-
-            "stato":
-
-                "attivo"
-
-                if self.attivo
-
-                else "errore",
-
-
-
-            "dispositivi":
-
-                self.gestore.elenco()
-
-                if self.gestore
-
-                else [],
-
-
-
-            "connessione":
-
-                self.connessione.stato()
-
-                if self.connessione
-
-                else {}
-
+            "nome": self.nome,
+            "stato": "attivo" if self.attivo else "errore",
+            "dispositivi": self.gestore.elenco() if self.gestore else [],
+            "connessione": self.connessione.stato() if self.connessione else {},
         }
