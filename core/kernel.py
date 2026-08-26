990104510a86dc8a6d66417c341fbd9bc6a8ee43
Fulmine1000 <simone.gionta93@gmail.com>
2026-08-26 22:21:35 +0200

    Aggiorna import dopo riordino struttura

--- core/kernel.py
+++ core/kernel.py
@@ -9,13 +9,13 @@ from core.dialogo import DialogoJarvis
 from core.automazioni import AutomazioniJarvis, PianificatoreJarvis
 from core.visione import VisioneJarvis
 from core.diagnostica import DiagnosticaJarvis
+from core.stato import StatoJarvis
+from core.sicurezza import SicurezzaJarvis
+from core.aggiornamenti import AggiornamentiJarvis
 from memoria.memoria import MemoriaJarvis
 from memoria.preferenze import PreferenzeJarvis
 from memoria.contesto import ContestoJarvis
 from personalita.personalita import PersonalitaJarvis
-from stato import StatoJarvis
-from sicurezza import SicurezzaJarvis
-from aggiornamenti import AggiornamentiJarvis
 from moduli.comandi_modulo import ModuloComandi
 from moduli.dispositivi_modulo import ModuloDispositivi
 from moduli.voce_modulo import ModuloVoce
@@ -66,12 +66,7 @@ class KernelJarvis:
         self.avvio = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         self.automazioni.avvia()
         self.pianificatore.avvia()
-        moduli = {
-            "Memoria": self.memoria,
-            "Dispositivi": self.modulo_dispositivi,
-            "Comandi": self.modulo_comandi,
-            "Voce": self.modulo_voce,
-        }
+        moduli = {"Memoria": self.memoria, "Dispositivi": self.modulo_dispositivi, "Comandi": self.modulo_comandi, "Voce": self.modulo_voce}
         for nome, modulo in moduli.items():
             self.manager.registra(nome, modulo)
         for nome in moduli:
@@ -105,31 +100,7 @@ class KernelJarvis:
         return True
 
     def stato_sistema(self):
-        return {
-            "nome": self.nome,
-            "versione": self.versione,
-            "stato": self.stato,
-            "base": self.base,
-            "avvio": self.avvio,
-            "arresto_richiesto": self.arresto_richiesto,
-            "voce_disponibile": self.voce_disponibile,
-            "memoria": self.memoria.stato(),
-            "voce": self.modulo_voce.stato(),
-            "comandi": self.modulo_comandi.stato(),
-            "dispositivi": self.modulo_dispositivi.stato(),
-            "capacita": self.capacita.stato(),
-            "dialogo_ai": self.dialogo.stato(),
-            "automazioni": self.automazioni.stato(),
-            "pianificatore": self.pianificatore.stato(),
-            "visione": self.visione.stato(),
-            "diagnostica": self.diagnostica.stato(),
-            "plugin": self.plugin_manager.stato(),
-            "moduli": self.manager.stato(),
-            "personalita": self.personalita.stato_personalita(),
-            "preferenze": self.preferenze.stato(),
-            "sicurezza": self.sicurezza.stato(),
-            "sistema": self.stato_sistema_modulo.completo(),
-        }
+        return {"nome": self.nome, "versione": self.versione, "stato": self.stato, "base": self.base, "avvio": self.avvio, "arresto_richiesto": self.arresto_richiesto, "voce_disponibile": self.voce_disponibile, "memoria": self.memoria.stato(), "voce": self.modulo_voce.stato(), "comandi": self.modulo_comandi.stato(), "dispositivi": self.modulo_dispositivi.stato(), "capacita": self.capacita.stato(), "dialogo_ai": self.dialogo.stato(), "automazioni": self.automazioni.stato(), "pianificatore": self.pianificatore.stato(), "visione": self.visione.stato(), "diagnostica": self.diagnostica.stato(), "plugin": self.plugin_manager.stato(), "moduli": self.manager.stato(), "personalita": self.personalita.stato_personalita(), "preferenze": self.preferenze.stato(), "sicurezza": self.sicurezza.stato(), "sistema": self.stato_sistema_modulo.completo()}
 
     def arresta(self):
         if self.stato == "Spento":
